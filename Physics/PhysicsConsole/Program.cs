75979780ab6c2db2aa222c03dfebd8c1884a760f
adfgl <69821550+adfgl@users.noreply.github.com>
2024-05-02 15:07:39 +0300

    set up repository

---
+++ Physics/PhysicsConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PhysicsConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Physics/PhysicsEngineLib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace PhysicsEngineLib
+{
+    public class Class1
+    {
+
+    }
+}
